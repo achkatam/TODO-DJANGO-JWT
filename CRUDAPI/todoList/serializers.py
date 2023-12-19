f4b7a2823c3cc75c6c43afe873e43e1374871dc5
achkatam <agmatevv@gmail.com>
2023-12-19 12:38:12 -0500

    email required

--- CRUDAPI/todoList/serializers.py
+++ CRUDAPI/todoList/serializers.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
